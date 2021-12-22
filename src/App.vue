f635fbfbfe9204118835ee81414a46c1b3657e37
zss <1912812429@qq.com>
2021-12-22 22:32:32 +0800

    texq

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>
+      有一只猪
+    </p>
+    <div>
+      <i class="iconfont">&#xe605;</i>
+      <i class="iconfont">&#xe610;</i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import './style/font_3043951_4zf2xddzw04/iconfont.css'
+export default {
+  name: 'App'
+}
+</script>
