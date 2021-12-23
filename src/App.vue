ef98041a5cf91ec1fae03132653aab806597305e
zss <1912812429@qq.com>
2021-12-23 23:37:10 +0800

    登录页面路由部分

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
   <div>
-    <p>
-      有一只猪
-    </p>
-    <div>
-      <i class="iconfont">&#xe605;</i>
-      <i class="iconfont">&#xe610;</i>
-      <van-button type="primary">主要按钮</van-button>
-      <van-button type="info">信息按钮</van-button>
-      <van-button type="default">默认按钮</van-button>
-      <van-button type="warning">警告按钮</van-button>
-      <van-button type="danger">危险按钮</van-button>
-    </div>
-    <p>hhhhhhhh</p>
+   <p>一直</p>
+   <router-link to="login">跳转</router-link>
+   <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
